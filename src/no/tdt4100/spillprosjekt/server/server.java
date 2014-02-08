7d908f1c6e0f1fc97fa6fcd686ee55f71aa8b94b
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2014-02-08 22:49:24 +0100

    Added server class

---
+++ src/no/tdt4100/spillprosjekt/server/server.java
@@ -0,0 +1,17 @@
+package no.tdt4100.spillprosjekt.server;
+
+/**
+ * Created by eiriksylliaas on 08.02.14.
+ */
+
+
+
+public class server {
+
+    public static void main (String[] args) {
+
+
+
+    }
+
+}
