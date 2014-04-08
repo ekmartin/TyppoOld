417eacd42f32f23ec552f0a6ed2183fabcb25ea3
Hans-Wilhelm Warlo <hans-wilhelm@warlo.no>
2014-04-08 22:38:23 +0200

    Moves variables and makes them static

--- src/no/tdt4100/spillprosjekt/game/Menu.java
+++ src/no/tdt4100/spillprosjekt/game/Menu.java
@@ -16,6 +16,9 @@ public class Menu extends BasicGameState {
     private Animation banner;
 
     public static Image backgroundImage;
+    public static Image backToMenu;
+    public static Image backToMenuHower;
+    public static MouseOverArea menuHower;
 
     private Image singlePlayerImage;
     private Image singlePlayerHowerImage;
@@ -31,8 +34,8 @@ public class Menu extends BasicGameState {
     public static TiledMap typeMap;
 
     public static Animation loadingAnimation;
-    private Image[] animationImage;
 
+    private Image[] animationImage;
     private Image[] buttonHoverImages;
     private Image[] bannerAnimationImages;
 
@@ -40,7 +43,8 @@ public class Menu extends BasicGameState {
     private MouseOverArea multiPlayerHower;
     private MouseOverArea[] mouseOverAreas;
 
-    private TypeFont typeFont;
+    public static TypeFont typeFont;
+    public static TypeFont typeFontBig;
 
     private int buttonX;
     private int buttonY = 350;
@@ -51,7 +55,8 @@ public class Menu extends BasicGameState {
 
         this.buttonX = container.getWidth() / 2;
 
-        typeFont = new TypeFont("Consolas", 32, true, java.awt.Color.white);
+        typeFont = new TypeFont("Consolas", 24, true, java.awt.Color.white);
+        typeFontBig = new TypeFont("Consolas", 64, true, java.awt.Color.white);
 
         typeSoundGood = new Sound(Thread.currentThread().getContextClassLoader().getResource("no/tdt4100/spillprosjekt/res/click.aif"));
         typeSoundFail = new Sound(Thread.currentThread().getContextClassLoader().getResource("no/tdt4100/spillprosjekt/res/Bottle.aif"));
@@ -79,6 +84,12 @@ public class Menu extends BasicGameState {
 
         int[] bannerDuration = {500, 500};
         banner = new Animation(bannerAnimationImages, bannerDuration, true);
+
+        backToMenu = new Image(Thread.currentThread().getContextClassLoader().getResourceAsStream("no/tdt4100/spillprosjekt/res/menu.png"), "menu.png", false);
+        backToMenuHower = new Image(Thread.currentThread().getContextClassLoader().getResourceAsStream("no/tdt4100/spillprosjekt/res/menu_hower.png"), "menu_hower.png", false);
+
+        menuHower = new MouseOverArea(container, backToMenu, (container.getWidth() / 2) - 96, 450);
+        menuHower.setMouseOverImage(backToMenuHower);
 
         singlePlayerImage = new Image(Thread.currentThread().getContextClassLoader().getResourceAsStream("no/tdt4100/spillprosjekt/res/single_player.png"), "single_player.png", false);
         singlePlayerHowerImage = new Image(Thread.currentThread().getContextClassLoader().getResourceAsStream("no/tdt4100/spillprosjekt/res/single_player_hover.png"), "single_player_hover.png", false);
@@ -115,7 +126,7 @@ public class Menu extends BasicGameState {
         if (button == 0) {
             if (singlePlayerHower.isMouseOver()) {
                 System.out.println("starting single");
-                stateGame.enterState(3, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
+                stateGame.enterState(6, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
             }
             else if (multiPlayerHower.isMouseOver()) {
                 stateGame.enterState(2, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
